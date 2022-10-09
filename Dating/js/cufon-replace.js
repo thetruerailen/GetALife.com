b101ea0004728897085fa08f9bb40c3c5b284af2
Keet DeMoss <103075692+thetruerailen@users.noreply.github.com>
2022-10-09 13:06:39 -0500

    Add files via upload

---
+++ Dating/js/cufon-replace.js
@@ -0,0 +1,5 @@
+Cufon.replace('.site-nav li', { fontFamily: 'Myriad Pro Regular', hover:true });
+Cufon.replace('h2, h3', { fontFamily: 'Myriad Pro Regular' });
+Cufon.replace('h2 b, h2 strong', { fontFamily: 'Myriad Pro Bold' });
+Cufon.replace('.box h3', { fontFamily: 'Myriad Pro Regular', textShadow:'1px 1px #aee5ef' });
+Cufon.replace('.form-box h3', { fontFamily: 'Myriad Pro Regular', textShadow:'1px 1px #e02323' });
